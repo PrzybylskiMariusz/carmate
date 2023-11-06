06778a62bfcf2656b7aea1a049aaebbbdf45315c
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2023-11-06 17:50:10 +0100

    Add favicon adn root structure

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<div>
+			<h1 className="h1-bold">Welcome to Carmate</h1>
+		</div>
+	);
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-	return (
-		<div>
-			<h1 className="h1-bold">Carmate is going to live on next.js</h1>
-		</div>
-	);
-}
